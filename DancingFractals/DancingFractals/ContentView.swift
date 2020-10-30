c33ff5f491880fe6fcca5e030db45e8fb555924a
Aline Borges <aline.borges@uber.com>
2020-10-30 15:56:58 -0300

    Create swift ui project

---
+++ DancingFractals/DancingFractals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DancingFractals
+//
+//  Created by Aline Borges on 30/10/20.
+//  Copyright © 2020 Aline Borges. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
